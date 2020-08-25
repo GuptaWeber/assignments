cce4f91f29c5878cd0e7e58a0dedea2eff1b2fd9
GUPTA PANDIRI <guptapandiri99@gmail.com>
2020-08-26 05:35:24 +0600

    Book Assignment 6

---
+++ java/BookAssgn6/HandleBook.java
@@ -0,0 +1,40 @@
+
+public class HandleBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BookStore bookStore = new BookStore();
+		
+		Book [] books = bookStore.getBooks();
+		
+		for(Book book : books){
+			book.display();
+			System.out.println("--------------------------------------------");
+		}
+		
+		Book findBook = bookStore.findBookById(books,123345);
+		
+		if(findBook.name!=null){
+			System.out.println("The name of the book is "+ findBook.name);
+		}else{
+			System.out.println("Book Not Found");
+		}
+		
+		Book updatePrice = bookStore.updateBook(books, 123345, 2999);
+		if(updatePrice.name!=null){
+			System.out.println("The name of the book is "+ updatePrice.name + " New Price is "+ updatePrice.price);
+		}else{
+			System.out.println("Book Not Found");
+		}
+		
+//		for(Book book : books){
+//			book.display();
+//			System.out.println("--------------------------------------------");
+//		}
+//		
+		
+
+	}
+
+}
